08826207c905772518166e33431eae629d209f34
Andres <andres.ahc@gmail.com>
2021-07-20 23:50:35 -0400

    Primer avance ev4

---
+++ Evaluacion4/Evaluacion4DAL/DAL/TipoDAL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evaluacion4DAL.DAL
+{
+    public class TipoDAL
+    {
+        public Evaluacion4BDEntities1 dbEntities = new Evaluacion4BDEntities1();
+
+        public List<Tipo> GetAll()
+        {
+            //Select*from Tipo
+            return dbEntities.Tipo.ToList();
+        }
+    }
+}
